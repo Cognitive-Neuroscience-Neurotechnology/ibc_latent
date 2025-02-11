4228dbd974c72fbbac27930720df0809b9471bc7
“hannah”1809 <hannah.01@arcor.de>
2025-02-11 23:12:37 +0100

    updated config

--- RSA/config.py
+++ RSA/config.py
@@ -1,2 +1,14 @@
+import os
 
-working_directory = '/home/hmueller2/Downloads/contrast_maps/resulting_smooth_maps_surface/'
+# Define the directories based on the hostname
+hostname = os.uname().nodename
+if hostname == 'nyx-login0.hpc.kyb.local':
+    base_dir = '/home/hmueller2/Downloads/contrast_maps/resulting_smooth_maps_surface/' # find the contrast maps here
+    output_dir = '/home/hmueller2/ibc_code/ibc_output'
+    code_dir = '/home/hmueller2/ibc_code/ibc_latent'
+else:
+    base_dir = '/Users/hannahmuller/nyx_mount/Downloads/contrast_maps/resulting_smooth_maps_surface/'
+    output_dir = '/Users/hannahmuller/nyx_mount/ibc_code/ibc_output'
+    code_dir = '/Users/hannahmuller/nyx_mount/ibc_code/ibc_latent'
+print(f"Base directory set to: {base_dir}")
+print(f"Output directory set to: {output_dir}")

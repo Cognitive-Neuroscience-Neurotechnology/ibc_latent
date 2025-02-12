963a774743175aaaf09ed3bdf205bb403c7854dd
“hannah”1809 <hannah.01@arcor.de>
2025-02-12 22:12:36 +0100

    Notes

---
+++ RSA/Parcellation/Glasser_to_fsaverage.py
@@ -0,0 +1,5 @@
+# I have downlaoded several files of the Glasser parcelation.
+# Separately I have a file that contains the information of which Glasser parcels belong to th Frontoparietal Network.
+# I want to move the Glasser atlas into fsaverage space (because my contrast maps are in fsaverage space).
+# I also then want to use the Frontoparietal Network parcels in fsaverage space for my further calculations.
+

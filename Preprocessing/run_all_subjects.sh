7a4198d076a97323808d2a6653a4d6d9be85fd99
“hannah”1809 <hannah.01@arcor.de>
2025-07-22 17:41:33 +0200

    turned into slurm job

--- Preprocessing/run_all_subjects.sh
+++ Preprocessing/run_all_subjects.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#SBATCH --job-name=ibc_preproc
+#SBATCH --output=logs/ibc_preproc_%j.out
+#SBATCH --error=logs/ibc_preproc_%j.err
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
 
 subjects=($(ls /home/hmueller2/ibc_data/sub-* | awk -F'-' '{print $NF}'))
 sessions=($(ls /home/hmueller2/ibc_data/sub-${subjects[0]}/ses-* | awk -F'-' '{print $NF}'))
@@ -7,9 +13,13 @@ directions=("ap" "pa")
 
 for subj in "${subjects[@]}"; do
   for sess in "${sessions[@]}"; do
-    echo "Running subject $subj session $sess"
-    singularity exec --home /home/hmueller2 \
-      /home/rglz/containers/gfae.sif \
-      bash /home/hmueller2/ibc_code/ibc_latent/Preprocessing/pipeline_preprocessing.sh "$subj" "$sess" "$task" "$direction"
+    for task in "${tasks[@]}"; do
+      for direction in "${directions[@]}"; do
+        echo "Running subject $subj session $sess task $task direction $direction"
+        singularity exec --home /home/hmueller2 \
+          /home/rglz/containers/gfae.sif \
+          bash /home/hmueller2/ibc_code/ibc_latent/Preprocessing/pipeline_preprocessing.sh "$subj" "$sess" "$task" "$direction"
+      done
+    done
   done
 done

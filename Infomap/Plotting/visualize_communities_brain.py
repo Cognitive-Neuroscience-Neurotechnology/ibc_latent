8c5cca53ceaaf2f7ed96b7e10999cccb544ad7b9
“hannah”1809 <hannah.01@arcor.de>
2025-08-29 15:57:02 +0200

    problems with visualizing .clu

--- Infomap/Plotting/visualize_communities_brain.py
+++ Infomap/Plotting/visualize_communities_brain.py
@@ -1,9 +1,36 @@
 """
-Take communities from infomap and plot them on the brains.
-
+Take communities from infomap and plot them on the brain like the k-means subnetworks.
 """
+
+# Example usage:
+# python3 /home/hmueller2/ibc_code/ibc_latent/Infomap/Plotting/visualize_communities_brain.py --subject 01 --dir /ptmp/hmueller2/Downloads --density 0.05
+
 import numpy as np
 import nibabel as nib
+import argparse
+import os
+
+img = nib.load('/home/Downloads/fmriprep_out/sub-01/anat/sub-01_space-fsLR_den-91k_thickness.dscalar.nii')
+print(img.get_fdata().shape)
+
+'''
+parser = argparse.ArgumentParser(description='Visualize Infomap communities on the brain.')
+parser.add_argument('--subject', help='do not include sub- prefix', required=True)
+parser.add_argument('--dir', help='Directory to study Data (until derivatives).', required=True)
+parser.add_argument('--density', help='Graph density for .clu file', default='0.05')
+args = parser.parse_args()
+
+derivative_dir = args.dir
+subject = args.subject
+density = args.density
+
+networks_dir = os.path.join(derivative_dir, "individual_networks", f'sub-{subject}', 'resting_state')
+output_dir = os.path.join(derivative_dir, "subnetworks", "infomap", f'sub-{subject}')
+os.makedirs(output_dir, exist_ok=True)
+
+clu_file = os.path.join(networks_dir, f'Bipartite_Density{density}.clu')
+template_cifti = os.path.join(networks_dir, f'sub-{subject}_ses-15_resting_concatenated_cleaned_smoothed_0.85_fsLR.dtseries.nii')
+output_cifti = os.path.join(output_dir, f'communities_density{density}.dscalar.nii')
 
 def load_clu(filename, skip_header=9):
     data = np.loadtxt(filename, skiprows=skip_header)
@@ -11,18 +38,16 @@ def load_clu(filename, skip_header=9):
     community_ids = data[:, 1].astype(int)
     return node_ids, community_ids
 
-clu_file = '/ptmp/hmueller2/Downloads/individual_networks/sub-01/resting_state/Bipartite_Density0.05.clu'
-template_cifti = '/ptmp/hmueller2/Downloads/individual_networks/sub-01/resting_state/sub-01_ses-15_resting_concatenated_cleaned_smoothed_0.85_fsLR.dtseries.nii'
-output_cifti = '/ptmp/hmueller2/Downloads/individual_networks/sub-01/resting_state/communities.dscalar.nii'
-
+print(f"Loading: {clu_file}")
 node_ids, community_ids = load_clu(clu_file)
 cifti = nib.load(template_cifti)
 n_grayordinates = cifti.get_fdata().shape[1]
 data = np.zeros(n_grayordinates, dtype=int)
 data[node_ids - 1] = community_ids
 
-# Create a new scalar header
-scalar_header = nib.cifti2.Cifti2Header.from_header((1, n_grayordinates), 'scalar')
-new_img = nib.Cifti2Image(data[np.newaxis, :], header=scalar_header)
+# Save as dscalar.nii using the template header
+new_img = nib.Cifti2Image(data[np.newaxis, :], header=cifti.header)
 nib.save(new_img, output_cifti)
 print(f"Saved: {output_cifti}")
+
+'''

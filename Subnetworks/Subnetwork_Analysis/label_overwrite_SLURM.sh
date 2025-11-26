8ef015b57cb2aa83e023e0fb2229d5040ce84caf
“hannah”1809 <hannah.01@arcor.de>
2025-11-26 17:35:08 +0100

    minor

---
+++ Subnetworks/Subnetwork_Analysis/label_overwrite_SLURM.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=relabeling
+#SBATCH --output=/ptmp/hmueller2/spider_logs/output/%A_%x_%a_%u.out
+#SBATCH --error=/ptmp/hmueller2/spider_logs/errors/%A_%x_%a_%u.err
+#SBATCH --partition=thin
+#SBATCH --exclusive=user
+#SBATCH --array=0-7   # Adjust to number of subjects
+#SBATCH --time=6:00:00
+#SBATCH --mail-type=FAIL,TIME_LIMIT
+
+container=/home/rglz/containers/gfae.sif
+working_dir=/ptmp/hmueller2/Downloads
+
+# Read subject from config file
+config_file=/ptmp/hmueller2/Downloads/subjects_resting.txt
+line=$(sed -n "$((SLURM_ARRAY_TASK_ID + 1))p" "$config_file")
+subject=$(echo "$line" | awk '{print $1}')
+
+echo "Processing subject: sub-${subject}"
+
+export APPTAINER_BIND="/run,/ptmp,/tmp,/opt/ohpc,/home/hmueller2"
+
+srun apptainer exec ${container} python /home/hmueller2/ibc_code/ibc_latent/Subnetworks/Subnetwork_Analysis/label_overwrite.py --subject ${subject}
+
+exit 0
+
+# run with: sbatch /home/hmueller2/ibc_code/ibc_latent/Subnetworks/Subnetwork_Analysis/label_overwrite_SLURM.sh
